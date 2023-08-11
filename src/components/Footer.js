12523f3b89de85f88c8b6ce33d8c57ed323019b6
Farhan Farrukh <rajafarhan97@gmail.com>
2023-08-12 04:36:38 +0500

    Inital Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; 2023 Farhan Farrukh.</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/MainBody.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function MainBody() {
+  return (
+    <main>
+      <section>
+        <h2>About Me</h2>
+        <p>
+          Welcome to My First React Application. I aspire to become Full Stack
+          Developer with a Focus on Front End and then implement the Web2
+          Technologies in Web3.
+        </p>
+      </section>
+      <section>
+        <h2>JS Libraries/Technologies I Intend to Learn in Near Future along with React & Angular</h2>
+        <ul>
+            <li><a href="https://graphql.org">GraphQL</a></li>
+            <li><a href="https://threejs.org">Three.js</a></li>
+            <li><a href="https://d3js.org">D3.js</a></li>
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default MainBody;
